52d2e7570fcd7b810e1868d2a95e3512bf57a3ba
jasonrenau <jasondu40160@gmail.com>
2023-05-25 15:18:27 +0200

    news

--- app.js
+++ app.js
@@ -2,9 +2,5 @@ const navbar = document.querySelector('.nav');
 const menu = document.querySelector('.hamburger');
 
 menu.addEventListener('click', function () {
-  if (navbar.classList.contains('showlinks')) {
-    navbar.classList.remove('showlinks');
-  } else {
-    navbar.classList.add('showlinks');
-  }
+  navbar.classList.toggle('showlinks');
 });
